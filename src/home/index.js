76394933d73ebbebc5308f5305896acc1596c7fd
TianJianSir <18721154047@163.com>
2018-06-30 16:25:30 +0800

    add image

--- src/home/index.js
+++ src/home/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Frame from '../component/Frame'
+import image_4 from '../assert/image_4.jpg'
 import './style.css'
 
 export default () => (
   <Frame>
     <div class='home-container'>
-      <h2>this is Home</h2>
+      <h2>
+        <p>this is Home</p>
+        <img src={image_4}/>
+      </h2>
     </div>
   </Frame>
 );
