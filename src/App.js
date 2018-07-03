0ea6b60b8da65b10866ec1cb501fa879ed308809
TianJianSir <18721154047@163.com>
2018-07-03 21:33:48 +0800

    代码分割+骨架图

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from './home';
-import About from './about';
-import Topics from './topics'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Loadable from 'react-loadable';
+import { Facebook } from 'react-content-loader'
+
+const Loading = () => <Facebook/>;
+
+const Home = Loadable({
+  loader: () => import('./home'),
+  loading: Loading,
+});
+
+const About = Loadable({
+  loader: () => import('./about'),
+  loading: Loading,
+});
+
+const Topics = Loadable({
+  loader: () => import('./topics'),
+  loading: Loading,
+});
 
 const APP = () => (
   <Router>
-      <div>
+      <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route path="/topics" component={Topics} />
-      </div>
+      </Switch>
   </Router>
 );
 
