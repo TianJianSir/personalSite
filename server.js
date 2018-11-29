54c37e97ae718bd04c69a245b84c1e890c3b56a8
jason.tian <jason.tian@ximalaya.com>
2018-11-29 15:09:57 +0800

    server 修改

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express  = require('express');
 const app = express();
 const port = 7000;
 
-app.use('/', express.static('build'))
 app.use('/static', express.static('build/static'))
+
+app.get('/', (req, res) => {
+    console.log('espress get request')
+    res.setHeader('Content-Type', 'text/html');
+    res.sendfile('./build/index.html')
+})
 
 app.listen(port,()=>console.log(`my_app listening on port ${port}!`))
