67de07dd4c4d00bea743ec344d10350a0df12e5b
jason.tian <jason.tian@ximalaya.com>
2018-11-29 16:30:21 +0800

    add log

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const port = 7000;
 
 app.use('/blog/static', express.static(path.join(__dirname, 'build/static')))
 
-app.get('/', (req, res) => {
-    console.log('espress get request')
-    res.setHeader('Content-Type', 'text/html');
-    res.sendfile('./build/index.html')
+app.use('*',function(req,res,next){
+    console.log('------->>>>>>')
+    console.log(req)
+    next()
 })
 
+// 因为在nginx上用blog做了反向代理
 app.get('/blog', (req, res) => {
     console.log('espress blog request')
     res.setHeader('Content-Type', 'text/html');
