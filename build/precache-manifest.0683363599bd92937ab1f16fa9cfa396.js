a92e387564e75f201d93e2195827d8e24a219dba
jason.tian <jason.tian@ximalaya.com>
2018-12-01 22:29:08 +0800

    build

---
+++ build/precache-manifest.0683363599bd92937ab1f16fa9cfa396.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "88ab8df7318d86f5981635740039236d",
+    "url": "/static/media/prize01.88ab8df7.png"
+  },
+  {
+    "revision": "787c5b2a211f68b6d88513ed565055e7",
+    "url": "/static/media/4.787c5b2a.jpg"
+  },
+  {
+    "revision": "1042f738be6c0c30bb50aebfbc15eb3c",
+    "url": "/static/media/3.1042f738.jpg"
+  },
+  {
+    "revision": "bb414b2033cbbbf853738baebb417857",
+    "url": "/static/media/2.bb414b20.jpg"
+  },
+  {
+    "revision": "436caf36f670882af62cec1ac78d5f9e",
+    "url": "/static/media/1.436caf36.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "db074eb0b49a0e67af69",
+    "url": "/static/js/main.db074eb0.chunk.js"
+  },
+  {
+    "revision": "7c0d7be02ce2745ea6f8",
+    "url": "/static/js/1.7c0d7be0.chunk.js"
+  },
+  {
+    "revision": "db074eb0b49a0e67af69",
+    "url": "/static/css/main.6800d874.chunk.css"
+  },
+  {
+    "revision": "92892a4ad8d62a234dd4ad0f7646a327",
+    "url": "/index.html"
+  }
+];
