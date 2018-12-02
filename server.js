4bc89f379cd923c73e0b919626d67265a8654b8a
jason.tian <jason.tian@ximalaya.com>
2018-12-02 18:45:57 +0800

    change server

--- server.js
+++ server.js
@@ -10,21 +10,21 @@ app.use('*',function(req,res,next){
 })
 
 // 本地使用
-app.use('/static', express.static('build/static'))
+// app.use('/static', express.static('build/static'))
 
-app.get('/', (req, res) => {
-    console.log('espress / request')
-    res.setHeader('Content-Type', 'text/html');
-    res.sendfile('./build/index.html')
-})
-
-// 服务器上启用
-// app.use('/blog/static', express.static('build/static'))
-
-// app.get('/blog', (req, res) => {
-//     console.log('espress blog request')
+// app.get('/', (req, res) => {
+//     console.log('espress / request')
 //     res.setHeader('Content-Type', 'text/html');
 //     res.sendfile('./build/index.html')
 // })
+
+// 服务器上启用
+app.use('/blog/static', express.static('build/static'))
+
+app.get('/blog', (req, res) => {
+    console.log('espress blog request')
+    res.setHeader('Content-Type', 'text/html');
+    res.sendfile('./build/index.html')
+})
 
 app.listen(port,()=>console.log(`my_app listening on port ${port}!`))
