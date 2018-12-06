bf5a278620a130aa84d97e74431242bda450fbb2
jason.tian <jason.tian@ximalaya.com>
2018-12-06 13:59:25 +0800

    add homepage

---
+++ build/precache-manifest.500bb7afef1f9cd9e00d2e65cc210c90.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "88ab8df7318d86f5981635740039236d",
+    "url": "/blog/static/media/prize01.88ab8df7.png"
+  },
+  {
+    "revision": "787c5b2a211f68b6d88513ed565055e7",
+    "url": "/blog/static/media/4.787c5b2a.jpg"
+  },
+  {
+    "revision": "1042f738be6c0c30bb50aebfbc15eb3c",
+    "url": "/blog/static/media/3.1042f738.jpg"
+  },
+  {
+    "revision": "bb414b2033cbbbf853738baebb417857",
+    "url": "/blog/static/media/2.bb414b20.jpg"
+  },
+  {
+    "revision": "436caf36f670882af62cec1ac78d5f9e",
+    "url": "/blog/static/media/1.436caf36.jpg"
+  },
+  {
+    "revision": "255cd0f7ab465faad54a",
+    "url": "/blog/static/js/runtime~main.255cd0f7.js"
+  },
+  {
+    "revision": "ed78328648c2d2fb35cf",
+    "url": "/blog/static/js/main.ed783286.chunk.js"
+  },
+  {
+    "revision": "81710041074e0edb68be",
+    "url": "/blog/static/js/1.81710041.chunk.js"
+  },
+  {
+    "revision": "ed78328648c2d2fb35cf",
+    "url": "/blog/static/css/main.bb3c23fb.chunk.css"
+  },
+  {
+    "revision": "640664e98b2555d0e9d8d74a5584849c",
+    "url": "/blog/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.edafdaee168c883d6c3489b3f586249f.js"
+  "/blog/precache-manifest.500bb7afef1f9cd9e00d2e65cc210c90.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/blog/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
